f1a5e564fd4c01016d7aee767b20bfa7ca259102
WilliamCappelletti <cappelletti.william@gmail.com>
2021-11-14 16:44:11 +0100

    init repository and fix requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis and development of methods for seizure detection and prediction',
+    author='William Cappelletti',
+    license='BSD-3',
+)
