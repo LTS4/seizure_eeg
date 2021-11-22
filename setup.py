4e1225e21ace9e3a6b50d8477bf8ad92393cf958
WilliamCappelletti <cappelletti.william@gmail.com>
2021-11-22 17:41:45 +0100

    format files

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='Analysis and development of methods for seizure detection and prediction',
-    author='William Cappelletti',
-    license='BSD-3',
+    version="0.1.0",
+    description="Analysis and development of methods for seizure detection and prediction",
+    author="William Cappelletti",
+    license="BSD-3",
 )
