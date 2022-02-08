c94a117c4e8af32fd53adeba897d57f7eb0f497f
WilliamCappelletti <cappelletti.william@gmail.com>
2022-02-08 11:14:09 +0100

    add config files

---
+++ seiz_eeg/config.py
@@ -0,0 +1,57 @@
+"""Dataclasses illustrating the content of the config file"""
+# pylint: disable=missing-class-docstring
+from dataclasses import dataclass
+from typing import Dict, List, Optional
+
+################################################################################
+# DATA CONFIG ##################################################################
+
+
+@dataclass
+class DataSourceConf:
+    version: str
+
+    source: str
+    force_download: bool
+
+    raw: str
+    raw_edf: str
+    raw_doc: str
+
+    processed: str
+
+    splits: List[str]
+
+    excluded_patients_dev: List[str]
+    excluded_patients_train: List[str]
+
+
+@dataclass
+class DataLabelsConf:
+    map: Dict[str, int]
+    binary: bool
+
+
+@dataclass
+class DataSignalsConf:
+    diff_channels: bool
+    sampling_rate: int  # Hz
+    clip_length: float  # seconds
+    clip_stride: float  # seconds
+    window_len: float  # seconds
+
+    # Interval [min, max] - include all with [null] or switch off with null
+    fft_coeffs: Optional[List[Optional[int]]]
+
+    node_level: bool
+
+
+@dataclass
+class DataConf:
+    raw_path: str
+    processed_path: str
+
+    tusz: DataSourceConf
+    labels: DataLabelsConf
+
+    signals: DataSignalsConf
--- seiz_eeg/tusz/main.py
+++ seiz_eeg/tusz/main.py
@@ -7,41 +7,36 @@ from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 from omegaconf import OmegaConf
 
-from src.config import Config
+from seiz_eeg.config import DataConf
 from seiz_eeg.tusz.download import download
 from seiz_eeg.tusz.io import write_parquet
 from seiz_eeg.tusz.process import process_walk
-from src.run import run
 
 ################################################################################
 # MAIN
 
-# @click.argument("raw-data-folder", type=click.Path(exists=True, path_type=Path))
-# @click.argument("processed-data-folder", type=click.Path(path_type=Path))
 
-
-@run(config_path="config.yaml")
-def main(cfg: Config):
+def main(cfg: DataConf):
     """Runs data processing scripts to turn raw data from (../raw) into
     cleaned data ready to be analyzed (saved in ../processed).
     """
     load_dotenv(find_dotenv())
 
-    raw_edf_folder = Path(cfg.data.tusz.raw_edf)
-    output_folder = Path(cfg.data.tusz.processed)
+    raw_edf_folder = Path(cfg.tusz.raw_edf)
+    output_folder = Path(cfg.tusz.processed)
 
     # Download data if missing folders
-    if cfg.data.tusz.force_download or (
-        not set(cfg.data.tusz.splits) <= {x.stem for x in raw_edf_folder.iterdir()}
+    if cfg.tusz.force_download or (
+        not set(cfg.tusz.splits) <= {x.stem for x in raw_edf_folder.iterdir()}
     ):
         logging.info("Downloading data")
         download(
-            source=cfg.data.tusz.source,
-            target=cfg.data.tusz.raw,
+            source=cfg.tusz.source,
+            target=cfg.tusz.raw,
             password=os.environ["NEDC_PASSWORD"],
         )
 
-    for split in cfg.data.tusz.splits:
+    for split in cfg.tusz.splits:
         logging.info("Creating %s dataset", split.upper())
 
         root_folder = raw_edf_folder / split
@@ -51,9 +46,9 @@ def main(cfg: Config):
         segments_df = process_walk(
             root_folder,
             signals_out_folder=signals_out_folder,
-            sampling_rate_out=cfg.data.signals.sampling_rate,
-            label_map=OmegaConf.to_container(cfg.data.labels.map),
-            binary=cfg.data.labels.binary,
+            sampling_rate_out=cfg.signals.sampling_rate,
+            label_map=OmegaConf.to_container(cfg.labels.map),
+            binary=cfg.labels.binary,
         )
 
         segments_save_path = output_folder / split / "segments.parquet"
