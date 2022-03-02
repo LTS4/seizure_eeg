e343ee76034ff53b250ae540e84d3d1335bc095b
WilliamCappelletti <cappelletti.william@gmail.com>
2022-03-02 10:29:18 +0100

    extract transform handling

--- seiz_eeg/dataset.py
+++ seiz_eeg/dataset.py
@@ -1,6 +1,6 @@
 """EEG Data class with common data retrieval"""
 import logging
-from typing import List, Optional, Tuple, Union
+from typing import Callable, List, Optional, Tuple, Union
 
 import numpy as np
 import torch
@@ -26,12 +26,9 @@ class EEGDataset(Dataset):
         *,
         clip_length: float,
         clip_stride: Union[int, float, str],
-        window_len: Optional[int] = -1,
+        signal_transform: Optional[Callable] = None,
         diff_channels: Optional[bool] = False,
-        fft_coeffs: Optional[Tuple[int, int]] = None,
         node_level: Optional[bool] = False,
-        mean: Optional[torch.Tensor] = None,
-        std: Optional[torch.Tensor] = None,
         device: Optional[str] = None,
     ) -> None:
         """Dataset of EEG clips with seizure labels
@@ -47,30 +44,13 @@ class EEGDataset(Dataset):
         logging.debug("Creating clips from segments")
         self.clips_df = make_clips(segments_df, clip_length=clip_length, clip_stride=clip_stride)
 
-        self.window_len = window_len
         self.diff_channels = diff_channels
-        self.fft_coeffs = fft_coeffs
-
         self.node_level(node_level)
-
         self.device = device
 
+        self.signal_transform = signal_transform
+
         self.output_shape = self._get_output_shape()
-
-        # Compute signals mean
-        if mean is None:
-            if std is not None:
-                raise ValueError("You passed std but no mean")
-
-            logging.debug("Computing stats (mean and std)")
-            self.mean, self.std = self._compute_stats()
-        else:
-            if std is None:
-                raise ValueError("You passed mean but no std")
-
-            logging.debug("Using predefined (mean and std)")
-            self.mean = mean
-            self.std = std
 
     def node_level(self, node_level: bool):
         """Setter for the node-level labels retrieval"""
@@ -113,36 +93,8 @@ class EEGDataset(Dataset):
         signals = torch.tensor(signals, dtype=torch.float32, device=self.device)
         label = torch.tensor(label, dtype=torch.long, device=self.device)
 
-        # 3. Optional: Split windows
-        if self.window_len > 0:
-            time_axis = 1
-            win_len = self.window_len * s_rate
-
-            signals = signals.reshape(
-                signals.shape[0] // win_len,  # nb of windows
-                win_len,  # nb of samples per window (time axis)
-                signals.shape[1],  # nb of signals
-            )
-        else:
-            time_axis = 0
-
-        # 3. Optional: Compute fft
-        if self.fft_coeffs:
-            # Define slices to extract
-            extractor = len(signals.shape) * [slice(None)]
-            extractor[time_axis] = slice(*self.fft_coeffs)
-
-            # Actual fft
-            signals = torch.abs(torch.fft.rfft(signals, axis=time_axis))
-            signals = signals[extractor]
-
-        # Center data. This is always performed, except for `_compute_mean`
-        if hasattr(self, "mean") and self.mean is not None:
-            signals -= self.mean
-
-            # Normalize data. This is always performed, except for `_compute_std`
-            if hasattr(self, "std") and self.std is not None:
-                signals /= self.std
+        if self.signal_transform is not None:
+            signals = self.signal_transform(signals)
 
         return signals, label
 
@@ -157,8 +109,6 @@ class EEGDataset(Dataset):
 
     def _compute_stats(self) -> Tuple[torch.Tensor, torch.Tensor]:
         """Compute mean and std of signals and store result in ``self.(mean|std)``"""
-        self.mean = None
-        self.std = None
 
         # This part is commented for reproducibility, consider reimplementing it if performances are
         # too slow
@@ -177,11 +127,11 @@ class EEGDataset(Dataset):
             t_sum_sq += X**2
 
         N = len(samples) * np.prod(self.output_shape[0])
-        self.mean = torch.sum(t_sum) / N
+        mean = torch.sum(t_sum) / N
         # Compute std with Bessel's correction
-        self.std = torch.sqrt((torch.sum(t_sum_sq) - N * self.mean**2) / (N - 1))
+        std = torch.sqrt((torch.sum(t_sum_sq) - N * self.mean**2) / (N - 1))
 
-        return self.mean, self.std
+        return mean, std
 
     def _get_output_shape(self) -> Tuple[torch.Size, torch.Size]:
         X0, y0 = self.__getitem__(0)
