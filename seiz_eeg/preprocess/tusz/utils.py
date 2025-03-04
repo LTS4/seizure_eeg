458442f422b6525b91413785d80c1b811e789ce3
WilliamCappelletti <cappelletti.william@gmail.com>
2025-03-04 16:05:08 +0100

    remove unused function

--- seiz_eeg/preprocess/tusz/utils.py
+++ seiz_eeg/preprocess/tusz/utils.py
@@ -1,6 +1,5 @@
 """Utility functions for data retrieval"""
-import re
-from datetime import datetime
+
 from typing import List
 
 import pandas as pd
@@ -24,14 +23,3 @@ def concat_labels(labels_list: List[dict]) -> DataFrame[LabelDF]:
 
 def check_label(label: str) -> str:
     return TYPOS.get(label, label)
-
-
-def extract_session_date(string: str) -> datetime:
-    """From a string of format ``s\\d{3}_YYYY_MM_DD`` extract date and session id."""
-    match = re.search(
-        r"s\d{3}_(\d{4}_\d{2}_\d{2})",
-        string,
-    )
-    date = datetime.strptime(match.group(1), "%Y_%m_%d")
-
-    return date
